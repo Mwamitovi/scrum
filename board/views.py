5c6d030c2c166118420b56d7e84f8226df81f13f
Martin Matovu <matovu.synergy@gmail.com>
2019-07-16 17:47:41 +0300

    Define 'SprintViewSet', inherits ModelViewSet which defines CRUD actions. These CRUD actions are bound to http methods while e instantiate the views.

--- board/views.py
+++ board/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# board/views.py
+from rest_framework import viewsets
+from board.models import Sprint
+from board.serializers import SprintSerializer
 
-# Create your views here.
+
+class SprintViewSet(viewsets.ModelViewSet):
+    """ API endpoint for listing and creating sprints """
+
+    queryset = Sprint.objects.order_by('end')
+    serializer_class = SprintSerializer
+
