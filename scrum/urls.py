f4407f2fdc099d2ee6fb341605fc9d9543d127af
Martin Matovu <matovu.synergy@gmail.com>
2019-07-16 16:18:37 +0300

    Refactor the scrum urlpatterns.  - remove the url() to admin.  - add url() which routes to rest_framework.authtoken.views.obtain_auth_token()

--- scrum/urls.py
+++ scrum/urls.py
@@ -13,9 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
+from django.conf.urls import url, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^api/token/', obtain_auth_token, name='api-token'),
 ]
