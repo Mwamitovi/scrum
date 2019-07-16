d932b7407c6409d42177e60344c14a34fec53169
Martin Matovu <matovu.synergy@gmail.com>
2019-07-16 16:38:49 +0300

    Define 'Sprint' model, with name, description and date fields

--- board/models.py
+++ board/models.py
@@ -1,3 +1,18 @@
+# board/models.py
+from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+@ python_2_unicode_compatible
+class Sprint(models.Model):
+    """ Development iteration period """
+
+    name = models.CharField(max_length=100, blank=True, default='')
+    description = models.TextField(blank=True, default='')
+    end = models.DateField(unique=True)
+
+    def __str__(self):
+        return self.name or _('Sprint ending %s') % self.end
+
