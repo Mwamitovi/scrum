65bedefa0e768846cad21e87ab30f9edc03fa476
Martin Matovu <matovu.synergy@gmail.com>
2019-07-16 16:14:04 +0300

    Update DATABASES.default.ENGINE to ``django.db.backends.postgresql`` (we are using PostgreSQL)

--- scrum/settings.py
+++ scrum/settings.py
@@ -74,8 +74,10 @@ WSGI_APPLICATION = 'scrum.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "scrum_db",
+        'USER': "root",
+        'PASSWORD': "root",
     }
 }
 
