bd3330e048e1bb11b64c860d769802fb0fed1551
Martin Matovu <matovu.synergy@gmail.com>
2019-07-17 20:06:30 +0300

    Register viewsets with the router for a given URL prefix

---
+++ board/urls.py
@@ -0,0 +1,9 @@
+# board/urls.py
+from rest_framework.routers import DefaultRouter
+from board import views
+
+
+router = DefaultRouter()
+router.register(r'sprints', views.SprintViewSet)
+router.register(r'tasks', views.TaskViewSet)
+router.register(r'users', views.UserViewSet)
