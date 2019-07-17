6e780e7bccfc15f6331c0023514b14724fd1d34a
Martin Matovu <matovu.synergy@gmail.com>
2019-07-17 20:25:55 +0300

    Include app (board) urls

--- scrum/urls.py
+++ scrum/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from rest_framework.authtoken.views import obtain_auth_token
+from board.urls import router
 
 urlpatterns = [
     url(r'^api/token/', obtain_auth_token, name='api-token'),
+    url(r'api/', include(router.urls)),
 ]
