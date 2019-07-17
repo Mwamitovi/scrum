5114c0779d54f9f575da5598c09bc1a222a8d737
Martin Matovu <matovu.synergy@gmail.com>
2019-07-17 20:49:08 +0300

    Add ``links`` to each serializer, a read-only field for the response body. Define get_links() method to build the related links (populates the links value)

--- board/serializers.py
+++ board/serializers.py
@@ -1,6 +1,7 @@
 # board/serializers.py
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 from board.models import Sprint, Task
 
 
@@ -8,32 +9,64 @@ User = get_user_model()
 
 
 class SprintSerializer(serializers.ModelSerializer):
+
+    links = serializers.SerializerMethodField('get_links')
+
     class Meta:
         model = Sprint
-        fields = ('id', 'name', 'description', 'end')
+        fields = ('id', 'name', 'description', 'end', 'links',)
+
+    def get_links(self, obj):
+        request = self.context['request']
+        return {
+            'self': reverse(
+                'sprint-detail', kwargs={'pk': obj.pk}, request=request
+            ),
+        }
 
 
+# noinspection PyMethodMayBeStatic
 class TaskSerializer(serializers.ModelSerializer):
 
     status_display = serializers.SerializerMethodField('get_status_display')
+    links = serializers.SerializerMethodField('get_links')
+    assigned = serializers.SlugRelatedField(
+        slug_field=User.USERNAME_FIELD, required=False
+    )
 
     class Meta:
         model = Task
         fields = ('id', 'name', 'description', 'sprint', 'status', 'status_display',
-                  'order', 'assigned', 'started', 'due', 'completed')
+                  'order', 'assigned', 'started', 'due', 'completed' 'links',)
 
-    @staticmethod
-    def get_status_display(obj):
+    def get_status_display(self, obj):
         return obj.get_status_display()
+
+    def get_links(self, obj):
+        request = self.context['request']
+        return {
+            'self': reverse(
+                'task-detail', kwargs={'pk': obj.pk}, request=request
+            ),
+        }
 
 
 class UserSerializer(serializers.ModelSerializer):
 
     full_name = serializers.CharField(source='get_full_name', read_only=True)
+    links = serializers.SerializerMethodField('get_links')
 
     class Meta:
         model = User
-        fields = ('id', User.USERNAME_FIELD, 'full_name', 'is_active')
+        fields = ('id', User.USERNAME_FIELD, 'full_name', 'is_active', 'links',)
+
+    def get_links(self, obj):
+        request = self.context['request']
+        return {
+            'self': reverse(
+                'user-detail', kwargs={User.USERNAME_FIELD: obj.username}, request=request
+            )
+        }
 
 
 
