a9cc527c0e427c80f6232cda81137c8680796fa9
Martin Matovu <matovu.synergy@gmail.com>
2019-07-17 18:29:53 +0300

    Define UserSerializer. We assume that any custom User model inherits from AbstractBaseUser, thus having the "is_active" attribute and "get_full_name" method

--- board/serializers.py
+++ board/serializers.py
@@ -27,6 +27,14 @@ class TaskSerializer(serializers.ModelSerializer):
         return obj.get_status_display()
 
 
+class UserSerializer(serializers.ModelSerializer):
+
+    full_name = serializers.CharField(source='get_full_name', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', User.USERNAME_FIELD, 'full_name', 'is_active')
+
 
 
 
