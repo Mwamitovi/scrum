2455a2ca1b47b53051b43293781257e2a3b56535
Martin Matovu <matovu.synergy@gmail.com>
2019-07-17 19:17:44 +0300

    Define TaskViewSet, and override 'queryset', 'serializer_class', and the pagination parameters

--- board/views.py
+++ board/views.py
@@ -1,9 +1,14 @@
 # board/views.py
+from django.contrib.auth import get_user_model
 from rest_framework import permissions, authentication
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.viewsets import ModelViewSet
-from board.models import Sprint
-from board.serializers import SprintSerializer
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+
+from board.serializers import SprintSerializer, TaskSerializer, UserSerializer
+from board.models import Sprint, Task
+
+
+User = get_user_model()
 
 
 class DefaultsMixin(object):
@@ -19,7 +24,7 @@ class DefaultsMixin(object):
     ]
 
 
-class SprintViewSet(DefaultsMixin, ModelViewSet, PageNumberPagination):
+class SprintViewSet(DefaultsMixin, PageNumberPagination, ModelViewSet):
     """
     API endpoint for listing and creating sprints
     """
@@ -28,4 +33,47 @@ class SprintViewSet(DefaultsMixin, ModelViewSet, PageNumberPagination):
     page_size = 25
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+
+class TaskViewSet(DefaultsMixin, PageNumberPagination, ModelViewSet):
+    """
+    API endpoint for listing and creating tasks
+    """
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    page_size = 25
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
