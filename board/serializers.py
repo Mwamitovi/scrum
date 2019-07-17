398177adce638b8bb0fbcabe3adef6a9ef170cad
Martin Matovu <matovu.synergy@gmail.com>
2019-07-17 17:58:44 +0300

    Define TaskSerializer, declare fields to display. We add get_status_display method too.

--- board/serializers.py
+++ board/serializers.py
@@ -1,10 +1,46 @@
 # board/serializers.py
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from board.models import Sprint
+from board.models import Sprint, Task
+
+
+User = get_user_model()
 
 
 class SprintSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Sprint
         fields = ('id', 'name', 'description', 'end')
+
+
+class TaskSerializer(serializers.ModelSerializer):
+
+    status_display = serializers.SerializerMethodField('get_status_display')
+
+    class Meta:
+        model = Task
+        fields = ('id', 'name', 'description', 'sprint', 'status', 'status_display',
+                  'order', 'assigned', 'started', 'due', 'completed')
+
+    @staticmethod
+    def get_status_display(obj):
+        return obj.get_status_display()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
