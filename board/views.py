e8dbb44c145cb27abd4c40d5a25a4c9cb89504d2
Martin Matovu <matovu.synergy@gmail.com>
2019-07-17 16:05:33 +0300

    Create DefaultMixin, to set default auth, perm and filter config.

--- board/views.py
+++ board/views.py
@@ -1,12 +1,29 @@
 # board/views.py
-from rest_framework import viewsets
+from rest_framework import permissions, authentication
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import ModelViewSet
 from board.models import Sprint
 from board.serializers import SprintSerializer
 
 
-class SprintViewSet(viewsets.ModelViewSet):
-    """ API endpoint for listing and creating sprints """
+class DefaultsMixin(object):
+    """
+    Default settings for view authentication, permissions, filtering
+    """
+    authentication_classes = [
+        authentication.BasicAuthentication,
+        authentication.TokenAuthentication,
+    ]
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
 
+
+class SprintViewSet(DefaultsMixin, ModelViewSet):
+    """
+    API endpoint for listing and creating sprints
+    """
     queryset = Sprint.objects.order_by('end')
     serializer_class = SprintSerializer
+
 
