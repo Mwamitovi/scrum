e2f8946bd744bf7c80514c6a2793dbf36e91fea7
Martin Matovu <matovu.synergy@gmail.com>
2019-07-17 21:08:10 +0300

    Refactor TaskSerializer, if task is assigned to a sprint and user, provide linkange using reverse()

--- board/serializers.py
+++ board/serializers.py
@@ -44,11 +44,22 @@ class TaskSerializer(serializers.ModelSerializer):
 
     def get_links(self, obj):
         request = self.context['request']
-        return {
+        links = {
             'self': reverse(
                 'task-detail', kwargs={'pk': obj.pk}, request=request
             ),
+            'sprint': None,
+            'assigned': None,
         }
+        if obj.sprint_id:
+            links['sprint'] = reverse(
+                'sprint-detail', kwargs={'pk': obj.sprint_id}, request=request
+            )
+        if obj.assigned:
+            links['assigned'] = reverse(
+                'user-detail', kwargs={User.USERNAME_FIELD: obj.assigned}, request=request
+            )
+        return links
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -67,21 +78,3 @@ class UserSerializer(serializers.ModelSerializer):
                 'user-detail', kwargs={User.USERNAME_FIELD: obj.username}, request=request
             )
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
