cd7e09937ba2f9b4a27b21553ad99ee3ccf93f6e
Martin Matovu <matovu.synergy@gmail.com>
2019-11-24 19:47:49 +0300

    Override the default `router.route` method to enforce login before any page access

--- board/static/board/js/router.js
+++ board/static/board/js/router.js
@@ -14,8 +14,36 @@
       let view = new app.views.HomepageView({ el: this.contentElement });
       this.render(view);
     },
+    route: function (route, name, callback) {
+      // we override default route to enforce login on every page
+      let login;
+      callback = callback || this[name];
+      callback = _.wrap(callback, function (original) {
+        let args = _.without(arguments, original);
+        
+        if (app.session.authenticate()) {
+          original.apply(this, args);
+        } else {
+          // show the login screen before calling the view
+          $(this.contentElement).hide();
+          // bind original callback once the login is successful
+          login = new app.views.LoginView();
+          $(this.contentElement).after(login.el);
+          login.on('done', function () {
+            $(this.contentElement).show();
+            original.apply(this, args);
+          }, this);
+          // render the login form
+          login.render();
+        }
+      });
+      return Backbone.Router.prototype.route.apply(
+        this, [route, name, callback]
+      );
+    },
     render: function (view) {
       if (this.current) {
+        this.current.undelegateEvents();
         this.current.$el = $();
         this.current.remove();
       }
