a1420291967b52f7f1d2a7f5266230594ed69b2a
Martin Matovu <matovu.synergy@gmail.com>
2019-11-24 18:31:50 +0300

    Add event to POST form data to `/api/token/` via app.apiLogin within `script#config`

--- board/static/board/js/views.js
+++ board/static/board/js/views.js
@@ -23,7 +23,51 @@
   
   let LoginView = TemplateView.extend({
     id: 'login',
-    templateName: '#login-template'
+    templateName: '#login-template',
+    errorTemplate: _.template('<span class="error"><%- msg %></span>'),
+    events: {
+      'submit form': 'submit'
+    },
+    submit: function (e) {
+      let data = {};
+      e.preventDefault();
+      this.form = $(e.currentTarget);
+      this.clearErrors();
+      data = {
+        username: $(':input[name="username"]', this.form).val(),
+        password: $(':input[name="password"]', this.form).val()
+      };
+      $.post(app.apiLogin, data)
+        .success($.proxy(this.loginSuccess, this))
+        .fail($.proxy(this.loginFailure, this));
+    },
+    loginSuccess: function (data) {
+      app.session.save(data.token);
+      this.trigger('login', data.token);
+    },
+    loginFailure: function (xhr, status, error) {
+      let errors = xhr.responseJSON;
+      this.showErrors(errors);
+    },
+    showErrors: function (errors) {
+      _.map(errors, function (fieldErrors, name) {
+        let field = $(':input[name=' + name + ']', this.form),
+            label = $('label[for=' + field.attr('id') + ']', this.form);
+        
+        if (label.length === 0) {
+          label = $('label', this.form).first();
+        }
+
+        function appendError(msg) {
+          label.before(this.errorTemplate({ msg: msg }));
+        }
+
+        _.map(fieldErrors, appendError, this);
+      }, this);
+    },
+    clearErrors: function () {
+      $('.error', this.form).remove();
+    }
   });
 
   app.views.HomepageView = HomepageView;
