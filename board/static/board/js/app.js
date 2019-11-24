3d25644d791e622d53bf6d360ede146869c5fcb1
Martin Matovu <matovu.synergy@gmail.com>
2019-11-24 14:08:18 +0300

    Instantiate a router once the DOM is ready within `app`

--- board/static/board/js/app.js
+++ board/static/board/js/app.js
@@ -1,6 +1,12 @@
+// board/js/app.js
+
 const app = (function ($) {
   let config = $('#config'),
       app = JSON.parse(config.text())
+  
+      $(document).ready(function () {
+        let router = new app.router();
+      });
 
   return app;
 })(jQuery);
