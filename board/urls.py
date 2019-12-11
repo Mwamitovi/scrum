d7b11366e3daadef263cbe166584d03e0539685a
Martin Matovu <matovu.synergy@gmail.com>
2019-12-11 18:17:25 +0300

    Refactor DRF router, since Backbone.Model.url() generates urls without trailing slash

--- board/urls.py
+++ board/urls.py
@@ -1,8 +1,8 @@
 # board/urls.py
 from rest_framework.routers import DefaultRouter
-from board import views
+from . import views
 
-router = DefaultRouter()
+router = DefaultRouter(trailing_slash=False)
 router.register(r'sprints', views.SprintViewSet)
 router.register(r'tasks', views.TaskViewSet)
 router.register(r'users', views.UserViewSet)
